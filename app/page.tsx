3ba9c28a9abd8909c21aa411a1edaa1556a4ed80
Jared Gentry <jcgentr@clemson.edu>
2024-06-29 17:27:12 -0400

    Flags finished. Now need sorting, searching, filtering, and design improvements

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,14 @@ export default function Home() {
       <ul className="grid grid-cols-auto-fit-minmax">
         {states.map((state) => (
           <li key={state.id} className="w-auto relative group bg-black">
-            <div className="relative flex group-hover:opacity-0 justify-center items-center transition-all duration-500 ease-in">
+            <div className="relative flex group-hover:opacity-0 justify-center items-center transition-all duration-300 ease-in">
               <Image
-                src={`images/flags/ar.svg`}
+                src={state.imageSrc}
                 alt={`${state.name} flag`}
                 width={600}
                 height={400}
                 className="opacity-80"
+                data-id={state.id}
               />
               <h1
                 className="absolute text-9xl font-black text-shadow"
@@ -22,13 +23,23 @@ export default function Home() {
                 {state.id}
               </h1>
             </div>
-            <div className="absolute top-0 hidden flex-col gap-1 group-hover:flex p-8">
-              <h1 className="text-2xl">{state.name}</h1>
+            <div className="w-full h-full absolute top-0 hidden flex-col gap-1 group-hover:flex p-8">
+              <div className="flex justify-between items-center">
+                <div className="text-2xl">{state.name}</div>
+                <div className="text-sm">
+                  {state.statehoodDate.toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                    timeZone: "UTC",
+                  })}
+                </div>
+              </div>
               <h2 className="text-base">Capital: {state.capital}</h2>
               <h3 className="text-sm">Nickname: {state.nickname}</h3>
               <h3 className="text-sm">Bird: {state.bird}</h3>
               <h3 className="text-sm">Population: {state.population}</h3>
-              <h3 className="text-sm">Total Area: {state.totalArea}</h3>
+              <h3 className="text-sm">Total Area: {state.totalArea} sq mi</h3>
             </div>
           </li>
         ))}
