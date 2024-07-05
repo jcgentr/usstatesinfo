bb98317ec84fbda004c16c8d263cc0e88eefce7e
Jared Gentry <jcgentr@clemson.edu>
2024-07-05 15:26:03 -0400

    Add search functionality

--- app/page.tsx
+++ app/page.tsx
@@ -22,39 +22,58 @@ export default function Home() {
   const [sortBy, setSortBy] = useState<
     "name" | "statehoodDate" | "capital" | "population" | "totalArea"
   >("name");
+  const [searchText, setSearchText] = useState("");
+
+  function handleSearch(e: any) {}
 
   const sortedStates = states.sort(sortByFxns[sortBy]);
+  const filteredStates = sortedStates.filter((s) =>
+    s.name.toLowerCase().includes(searchText.toLowerCase())
+  );
 
   return (
     <main className="bg-slate-900 text-white">
-      <div className="p-4 flex items-center">
-        <label htmlFor="sortOptions">Sort by</label>
-        <select
-          id="sortOptions"
-          name="sortOptions"
-          className="ml-4 rounded text-slate-900"
-          value={sortBy}
-          onChange={(e) =>
-            setSortBy(
-              e.currentTarget.value as
-                | "name"
-                | "statehoodDate"
-                | "capital"
-                | "population"
-                | "totalArea"
-            )
-          }
-        >
-          <option value="name">State Name</option>
-          <option value="statehoodDate">Statehood Date</option>
-          <option value="capital">Capital Name</option>
-          <option value="population">Population</option>
-          <option value="totalArea">Total Area</option>
-        </select>
+      <div className="p-4 flex justify-between items-center">
+        <div>
+          <label htmlFor="sortOptions">Sort by</label>
+          <select
+            id="sortOptions"
+            name="sortOptions"
+            className="ml-4 rounded text-slate-900"
+            value={sortBy}
+            onChange={(e) =>
+              setSortBy(
+                e.currentTarget.value as
+                  | "name"
+                  | "statehoodDate"
+                  | "capital"
+                  | "population"
+                  | "totalArea"
+              )
+            }
+          >
+            <option value="name">State Name</option>
+            <option value="statehoodDate">Statehood Date</option>
+            <option value="capital">Capital Name</option>
+            <option value="population">Population</option>
+            <option value="totalArea">Total Area</option>
+          </select>
+        </div>
+        <div>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search..."
+            className="rounded text-slate-900 px-2"
+            value={searchText}
+            onChange={(e) => setSearchText(e.currentTarget.value)}
+          />
+        </div>
       </div>
       <ul className="grid grid-cols-auto-fit-minmax">
-        {sortedStates.map((state) => (
-          <li key={state.id} className="w-auto relative group">
+        {filteredStates.map((state) => (
+          <li key={state.id} className="w-auto relative group m-auto">
             <div className="relative flex group-hover:opacity-0 justify-center items-center transition-all duration-300 ease-in">
               <Image
                 src={state.imageSrc}
@@ -71,7 +90,7 @@ export default function Home() {
                 {state.id}
               </h1>
             </div>
-            <div className="w-full h-full absolute top-0 hidden flex-col gap-1 group-hover:flex p-8">
+            <div className="w-full h-full absolute top-0 hidden flex-col gap-1 group-hover:flex p-8 max-w-[38rem]">
               <div className="flex justify-between items-center">
                 <div className="text-2xl">{state.name}</div>
                 <div className="text-sm">
